26e10f7fdadc66b41c888d65c5b4f8b766ab6f72
Dat Do <datdo@rjc.co.jp>
2018-07-09 18:34:11 +0900

    Change location for production

--- go_production.sh
+++ go_production.sh
@@ -8,22 +8,22 @@ echo 'Upload to server'
 rsync --rsh="/usr/bin/ssh" \
       --times \
       ./build/index.html \
-      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/endpoint
+      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/docs/api/v1/ja
 rsync --rsh="/usr/bin/ssh" \
       --times \
       ./build/fonts/* \
-      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/fonts
+      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/docs/api/v1/fonts
 rsync --rsh="/usr/bin/ssh" \
       --times \
       ./build/images/* \
-      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/images
+      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/docs/api/v1/images
 rsync --rsh="/usr/bin/ssh" \
       --times \
       ./build/javascripts/* \
-      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/javascripts
+      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/docs/api/v1/javascripts
 rsync --rsh="/usr/bin/ssh" \
       --times \
       ./build/stylesheets/* \
-      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/stylesheets
+      ubuntu@api.wealth-park.com:/home/ubuntu/wealthpark-api/server/public/docs/api/v1/stylesheets
 
 echo 'DONE'
